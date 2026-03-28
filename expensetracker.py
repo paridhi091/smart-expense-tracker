9f84659909f3d07720fa65e672fae299deb43303
Paridhi Rathi <paridhi.25bai11559@vitbhopal.ac.in>
2026-03-28 21:41:27 +0530

    Added basic Streamlit UI

--- expensetracker.py
+++ expensetracker.py
@@ -1,1 +1,9 @@
-print("expense tracker")
+import streamlit as st
+
+st.title("Smart Expense Tracker AI ")
+
+amount = st.number_input("Enter amount")
+category = st.text_input("Enter category")
+
+if st.button("Add Expense"):
+    st.write(f"Added: ₹{amount} for {category}")
