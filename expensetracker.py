553df517a37e14165e513d35a2924442a2ac139f
Paridhi Rathi <paridhi.25bai11559@vitbhopal.ac.in>
2026-03-31 11:08:45 +0530

    updated

--- expensetracker.py
+++ expensetracker.py
@@ -41,6 +41,27 @@ if os.path.exists(FILE_NAME):
     total = df["Amount"].sum()
     st.metric("Total Spending", f"₹ {total}")
 
+    # 📊 Smart Insights
+if os.path.exists(FILE_NAME):
+    df = pd.read_csv(FILE_NAME)
+
+    st.subheader(" Smart Insights")
+
+    # 1. Highest spending category
+    top_category = df.groupby("Category")["Amount"].sum().idxmax()
+    top_amount = df.groupby("Category")["Amount"].sum().max()
+    st.write(f" You spend the most on **{top_category}** (₹{top_amount})")
+
+    # 2. Average expense
+    avg = df["Amount"].mean()
+    st.write(f" Your average expense is ₹{avg:.2f}")
+
+    # 3. Simple warning
+    if avg > 1000:
+        st.warning(" Your average spending is high!")
+    else:
+        st.success(" Your spending is under control")
+
 
     if len(df) > 1:
         df["Day"] = range(1, len(df) + 1)
@@ -54,7 +75,7 @@ if os.path.exists(FILE_NAME):
         future_day = np.array([[len(df) + 1]])
         prediction = model.predict(future_day)
 
-        st.subheader("🔮 Predicted Next Expense")
+        st.subheader(" Predicted Next Expense")
         st.write(f"₹ {prediction[0]:.2f}")
     else:
         st.info("Add at least 2 expenses for prediction")
